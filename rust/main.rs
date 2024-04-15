c6161833660b419a81d5a1f11350e9291e0c8534
ritzle <vladislav.ryazanov.study@gmail.com>
2024-04-15 10:41:58 +0700

    add

---
+++ rust/main.rs
@@ -0,0 +1,37 @@
+fn main(){
+
+    let task : i32;
+
+
+    match task {
+        1 =>{
+            task1();
+        }
+
+        2 =>{
+            task2();
+        }
+
+        3 =>{
+            task3();
+        }
+
+
+
+
+    }
+
+
+}
+
+fn task1(){
+
+}
+
+fn task2(){
+
+}
+
+fn task3(){
+
+}
